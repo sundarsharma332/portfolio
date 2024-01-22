0c2fe296a18c7e56c48ec0b81ddbe99a4391dbcc
sundarsharma332 <sundarsharma469@gmail.com>
2024-01-22 12:11:24 +0530

    added the analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Head from "./head";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import { Analytics } from '@vercel/analytics/react';
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +19,7 @@ export default function RootLayout({
             <body className={`${inter.className} flex flex-col h-screen justify-between`}>
                <Navbar/>
                {children}
+               <Analytics />
                 <Footer />
             </body>
         </html>
