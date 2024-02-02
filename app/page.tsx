df89c1d2563f612097c69a8fea81e0d4a36761f0
sundarsharma332 <sundarsharma469@gmail.com>
2024-02-02 23:31:56 +0530

    revert back

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,6 @@ import CaseStudyLists from "./components/CaseStudy";
 import CodingSoftwaresUsed from "./components/CodingSoftware";
 import HeroSection from "./components/HeroSection";
 import ToolsUsed from "./components/SoftwareCollections";
-import BackgroundBeamsDemo from "./components/beams";
-import { GoogleGeminiEffectDemo } from "./components/gemini";
-import { GoogleGeminiEffect } from "./components/google-gemini-effect";
 import HireMe from "./components/idea";
 
 export default function Home() {
@@ -15,12 +12,10 @@ export default function Home() {
       <>
       <main className="mb-auto">
         <HeroSection />
-        {/* <GoogleGeminiEffectDemo/> */}
         <CaseStudyLists/>
         <ToolsUsed/>
         <CodingSoftwaresUsed/>
         <HireMe/>
-       {/* <BackgroundBeamsDemo/> */}
       </main>
       </>
   )
